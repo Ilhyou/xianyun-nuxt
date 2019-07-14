1d8dbc5dd4a1e7116b16d18bd49f805686dc07cc
齐林河 <1277859500@qq.com>
2019-07-14 21:22:38 +0800

    城市的模糊查询以及默认选中第一个城市

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -8,8 +8,8 @@
     <!-- 搜索广告栏 -->
     <el-row type="flex" justify="space-between">
       <!-- 搜索表单 -->
-      <div>搜索</div>
-
+      <!-- <div>搜索</div> -->
+      <SearchForm />
       <!-- banner广告 -->
       <div class="sale-banner">
         <img src="http://157.122.54.189:9093/images/pic_sale.jpeg" />
@@ -43,7 +43,12 @@
 </template>
 
 <script>
-export default {};
+import SearchForm from "@/components/air/searchForm";
+export default {
+  components: {
+    SearchForm
+  }
+};
 </script>
 
 <style scoped lang="less">
